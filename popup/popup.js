496aa0393542bf45f6374ab94c1f90f840aeccfa
OneLessPuppy <21loganmeeks@gmail.com>
2024-08-09 14:26:39 -0700

    Manifest and temporary popup creation

---
+++ popup/popup.js
@@ -0,0 +1,11 @@
+console.log("This is a popup!");
+
+
+// 1. Gather info from Jotform or Google Forms
+
+// 2. Open a new window or tab with a new gmail draft and enter the correct message to the hospital
+
+// 3. Open a new window or tab with a google doc to be printed
+
+// 4. Potentially autofill data to a google sheet or Sarena's new drug log site.
+
